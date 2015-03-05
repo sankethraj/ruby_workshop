42ab042fdf4dc433fe996ff28dcc745f9338fe89
root <root@201011JUNC0407.(none)>
2015-03-05 15:14:19 +0530

    removed additions  Please enter the commit message for your changes. Lines starting

--- workshop/add.rb
+++ workshop/add.rb
@@ -1,9 +1,4 @@
-puts 4+3
-puts 4-3
-puts 4*3
-print 4+3
-print  "\n"
-print 100*4
 first_name = "sanketh"
+last_name = "raj"
 fav_color ="red"
-puts"hi,my name is " + first_name ".my fav color is" +fav_color
+puts"hi,my name is " + first_name + last_name+ ".my fav color is" +fav_color
